3114f0bc04810251c59c5695753927afc398b850
Kyle Bartz <mail@kyle.fail>
2025-11-09 00:07:19 +0000

    test

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -1,5 +1,15 @@
-{
-  hosts.nixos.iso = {
-    networking.hostName = "iso";
+{lib, config, ...}: {
+  hosts.nixos.iso.module = {
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+    isoImage = {
+      makeEfiBootable = true;
+      makeUsbBootable = true;
+      squashfsCompression = "zstd -Xcompression-level 3";
+    };
+
+    imports = with config.flake.modules.nixos; [
+      core
+    ];
   };
 }
--- parts/host-configurations.nix
+++ parts/host-configurations.nix
@@ -1,14 +1,17 @@
-{lib, ...}: {
+{lib, config, ...}: {
   options.hosts.nixos = lib.mkOption {
     type = lib.types.lazyAttrsOf (
       lib.types.submodule {
-        options = lib.mkOption {
+        options.module = lib.mkOption {
           type = lib.types.deferredModule;
-          default = {};
         };
       }
     );
   };
 
-  config = { };
+  config.flake = {
+    nixosConfigurations = lib.flip lib.mapAttrs config.hosts.nixos (
+      name: { module }: lib.nixosSystem { modules = [ module ]; }
+    );
+  };
 }
