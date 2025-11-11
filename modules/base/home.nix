4a757ad2a00432ea178b570731d437784df81d16
Kyle Bartz <mail@kyle.fail>
2025-11-11 20:01:50 +0000

    renamed homeManager to home

--- modules/base/home.nix
+++ modules/base/home.nix
@@ -6,6 +6,7 @@
     home = {
       stateVersion = lib.mkDefault "25.05";
       sessionPath = ["$HOME/.local/bin"];
+      sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
     };
 
     programs.home-manager.enable = true;
