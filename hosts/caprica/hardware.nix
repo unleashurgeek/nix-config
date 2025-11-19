1aef2ef9a3aa3a4191388b1b74e6211aa2f3258c
Kyle Bartz <mail@kyle.fail>
2025-11-18 16:36:11 -0800

    Enabled graphics + 32bit graphics on caprica

--- hosts/caprica/hardware.nix
+++ hosts/caprica/hardware.nix
@@ -2,5 +2,10 @@
   hosts.nixos.caprica.module = {inputs, ...}: {
     imports = [inputs.nixos-facter-modules.nixosModules.facter];
     facter.reportPath = ./facter.json;
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
   };
 }
