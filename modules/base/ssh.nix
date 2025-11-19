80f220c3ca22b9db72ba91d55497fa90c353010e
Kyle Bartz <mail@kyle.fail>
2025-11-18 16:41:14 -0800

    Added enableAllTerminfo to base

--- modules/base/ssh.nix
+++ modules/base/ssh.nix
@@ -34,6 +34,10 @@
       ];
     };
 
+    # Add terminfo database of all known terminals to the system profile.
+    # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/nixos/modules/config/terminfo.nix
+    environment.enableAllTerminfo = true;
+
     programs.ssh = {
       knownHosts =
         builtins.mapAttrs (name: _: {
